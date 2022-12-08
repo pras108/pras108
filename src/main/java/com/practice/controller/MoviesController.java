7f65a251d6c0cc2aa7c0575dfb1f9f93bc4498ff
pras108 <prashantshandilya108@gmail.com>
2022-12-08 17:09:46 +0530

    added pagination and basic exception handling

--- src/main/java/com/practice/controller/MoviesController.java
+++ src/main/java/com/practice/controller/MoviesController.java
@@ -4,9 +4,12 @@ import com.practice.model.Movies;
 import com.practice.service.MovieService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -25,5 +28,19 @@ public class MoviesController {
         List<Movies> allMoviesFromDB = movieService.getAllMoviesFromDB();
         log.info("size of movies list: {}", allMoviesFromDB.size());
         return new ResponseEntity<>(allMoviesFromDB.toString(), HttpStatus.OK);
+    }
+
+    @GetMapping("/pagedMovies")
+    public ResponseEntity<String> getPagedMovies(@RequestParam("pageNo") int pageNo,
+                                                 @RequestParam("size") int size) throws Exception{
+        Page<Movies> pagedResultsForMovies = movieService.getPagedResultsForMovies(pageNo, size);
+        log.info("size of paged movies : {}", pagedResultsForMovies.stream().count());
+        return new ResponseEntity<>(pagedResultsForMovies.getContent().toString(), HttpStatus.OK);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> throwGenericException(Exception exception) {
+        log.error(exception.getMessage());
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/practice/service/MovieService.java
+++ src/main/java/com/practice/service/MovieService.java
@@ -3,6 +3,8 @@ package com.practice.service;
 import com.practice.model.Movies;
 import com.practice.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,5 +17,10 @@ public class MovieService {
 
     public List<Movies> getAllMoviesFromDB() {
         return movieRepository.findAll();
+    }
+
+    public Page<Movies> getPagedResultsForMovies(int pageNo, int size) {
+        PageRequest pageRequest = PageRequest.of(pageNo, size);
+        return movieRepository.findAll(pageRequest);
     }
 }
